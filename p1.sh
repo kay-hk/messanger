d2a7c36857be77ac5b687e7058dc37353c5f8c0c
kay-hk <kay.kazmierczak@outlook.com>
2025-01-21 14:02:15 +0000

    searchContact function

--- p1.sh
+++ p1.sh
@@ -76,6 +76,16 @@ updateContact() {
     fi
 }
 
+searchContacts() {
+    read -r -p "Name of contact to search: " cname
+        if grep -i -q "^$cname," "$contacts"; then
+            contact=$(grep "^$cname," "$contacts")
+            echo "Found Contact: $contact"
+        else
+            echo "No contact found with the name '$cname'."
+        fi
+}
+
 while true; do
     if [ ! -f $contacts ]; then
         touch $contacts
